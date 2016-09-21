54265fac6f4a79eb7cf1ae2518c001a8915d9818
Evgeny <evgenyjdk@gmail.com>
2016-09-21 14:22:18 +0300

    test + add CommentStorage

---
+++ Night Life/resources/Fonts/CommentStorage.swift
@@ -0,0 +1,13 @@
+//
+//  CommentStorage.swift
+//  GlobBar
+//
+//  Created by Evgeny on 9/21/16.
+//  Copyright © 2016 com.NightLife. All rights reserved.
+//
+
+import Foundation
+
+class CommentStorage {
+    
+}
