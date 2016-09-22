878c93456a1980e0eab148460cffdb3819795963
Administrator <evgenyjdk@gmail.com>
2016-09-21 17:50:40 -0700

    added ComentViewModel

---
+++ Night Life/sources/ViewModel/CommentViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  CommentViewModel.swift
+//  GlobBar
+//
+//  Created by Administrator on 21.09.16.
+//  Copyright © 2016 com.NightLife. All rights reserved.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+import RxDataSources
+
+class CommentViewModel {
+    
+}
